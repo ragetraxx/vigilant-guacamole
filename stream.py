3f056a9a3c889578a55e6c1b592a23c2be4f2c61
ragetraxx <93855827+ragetraxx@users.noreply.github.com>
2025-03-13 11:11:20 +0800

    Update stream.py

--- stream.py
+++ stream.py
@@ -24,7 +24,9 @@ def stream_movie(movie):
     command = f"""
     ffmpeg -re -fflags +genpts -rtbufsize 128M -probesize 10M -analyzeduration 1000000 \
     -i {video_url_escaped} -i {overlay_path_escaped} \
-    -filter_complex "[1:v]scale2ref=w=main_w:h=main_h[ovr][base];[base][ovr]overlay=0:0,drawtext=text='{overlay_text}':fontcolor=white:fontsize=24:x=20:y=20" \
+    -filter_complex "[1:v]scale='if(gt(a,main_w/main_h),main_w,-1)':'if(gt(a,main_w/main_h),-1,main_h)'[ovr]; \
+                     [0:v][ovr]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2, \
+                     drawtext=text='{overlay_text}':fontcolor=white:fontsize=24:x=20:y=20" \
     -c:v libx264 -preset ultrafast -tune zerolatency -b:v 2500k -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 50 \
     -c:a aac -b:a 192k -ar 48000 -f flv {shlex.quote(RTMP_URL)}
     """
